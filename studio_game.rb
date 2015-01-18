c17ae7e7101625e487364bb3785c19d6ac01bb58
Marcin Drozd <marcindrozd@me.com>
2015-01-18 13:02:17 +0100

    Finished exercises on strings and numbers.

--- studio_game.rb
+++ studio_game.rb
@@ -1,7 +1,6 @@
-greeting = "Welcome!"
+player1_name = "larry"
+player1_health = 60
+player2_name = "curly"
+player3_name = "moe"
 
-3.times do
-  puts greeting   
-end
-
-puts Time.now
+puts "Players:\n\t#{player1_name}\n\t#{player2_name}\n\t#{player3_name}"
