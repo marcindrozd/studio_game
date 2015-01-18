2821da9d1979a2c2aac6cce61bea93842412409a
Marcin Drozd <marcindrozd@me.com>
2015-01-18 13:40:26 +0100

    Completed variables and objects exercise.

--- studio_game.rb
+++ studio_game.rb
@@ -1,6 +1,16 @@
 player1_name = "larry"
 player1_health = 60
 player2_name = "curly"
+player2_health = 125
 player3_name = "moe"
+player3_health = 100
+player4_name = "shemp"
+player4_health = 90
 
-puts "Players:\n\t#{player1_name}\n\t#{player2_name}\n\t#{player3_name}"
+puts "#{player1_name.capitalize} has health of #{player1_health}."
+puts "#{player2_name.upcase} has health of #{player2_health}."
+puts "#{player3_name.capitalize} has health of #{player3_health}".center(40, "*")
+puts "#{player4_name.capitalize.ljust(20, '.')} #{player4_health} health"
+
+current_time = Time.new
+puts "The game started on #{current_time.strftime('%A %d/%m/%Y at %I:%M%p')}"
