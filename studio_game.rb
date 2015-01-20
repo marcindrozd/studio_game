8c36234b9773588676fb161888cc51fbbbd785b7
Marcin Drozd <marcindrozd@me.com>
2015-01-20 10:46:41 +0100

    Completed Arrays.

--- studio_game.rb
+++ studio_game.rb
@@ -30,11 +30,16 @@ player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-puts player1.health
-puts player2.health
-puts player3.health
+players = [player1, player2, player3]
 
-player2.name = "Lawrence"
+puts "There are #{players.size} players in the game:"
+players.each do |p|
+  puts p
+end
 
-puts player2.name
-puts player2
+players.each do |p|
+  p.w00t
+  p.blam
+  p.w00t
+  puts p
+end
