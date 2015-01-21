672cab43b63a73ae4ebf1840e56264de1019cb31
Marcin Drozd <marcindrozd@me.com>
2015-01-21 12:33:28 +0100

    Moved classes into separate files.

---
+++ game.rb
@@ -0,0 +1,27 @@
+require_relative 'player'
+
+class Game
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def play
+    puts "There are #{@players.size} players in #{title}:"
+    puts @players
+    puts
+
+    @players.each do |player|
+      player.blam
+      player.w00t
+      player.w00t
+      puts player
+    end
+  end
+end
--- studio_game.rb
+++ studio_game.rb
@@ -1,56 +1,5 @@
-class Player
-  attr_reader :health
-  attr_accessor :name
-
-  def initialize(name, health=100)
-    @name = name.capitalize
-    @health = health
-  end
-
-  def score
-    @health + @name.length
-  end
-
-  def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{score}."
-  end
-
-  def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
-  end
-
-  def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
-  end
-end
-
-class Game
-  attr_reader :title
-
-  def initialize(title)
-    @title = title
-    @players = []
-  end
-
-  def add_player(player)
-    @players << player
-  end
-
-  def play
-    puts "There are #{@players.size} players in #{title}:"
-    puts @players
-    puts
-
-    @players.each do |player|
-      player.blam
-      player.w00t
-      player.w00t
-      puts player
-    end
-  end
-end
+require_relative 'player'
+require_relative 'game'
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
