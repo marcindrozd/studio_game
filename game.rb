40575c1bdd11a2e20e7db8196c1e02087e335904
Marcin Drozd <marcindrozd@me.com>
2015-01-24 12:57:35 +0100

    Add treasure trove module

--- game.rb
+++ game.rb
@@ -1,6 +1,7 @@
 require_relative 'player'
 require_relative 'die'
 require_relative 'game_turn'
+require_relative 'treasure_trove'
 
 class Game
   attr_reader :title
@@ -43,6 +44,12 @@ class Game
     puts "There are #{@players.size} players in #{title}:"
     puts @players
     puts
+
+    treasures = TreasureTrove::TREASURES
+    puts "There are #{treasures.size} treasures in the Treasure Trove:"
+    treasures.each do |treasure|
+      puts "A #{treasure.name} is worth #{treasure.points} points"
+    end
 
     1.upto(rounds) do |round|
       puts "\nRound #{round}:"
