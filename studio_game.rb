146e2791daebf59d7ce13d2c1a536d96e9693df1
Marcin Drozd <marcindrozd@me.com>
2015-01-26 11:15:21 +0100

    Completed inheritance

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,7 @@
 require_relative 'player'
 require_relative 'game'
+require_relative 'clumsy_player'
+require_relative 'berserker_player'
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
@@ -7,6 +9,10 @@ player3 = Player.new("curly", 125)
 
 knuckleheads = Game.new("Knuckleheads")
 knuckleheads.load_players(ARGV.shift || "players.csv")
+player4 = ClumsyPlayer.new("klutz", 105)
+knuckleheads.add_player(player4)
+player5 = BerserkPlayer.new("berserker", 50)
+knuckleheads.add_player(player5)
 
 
 loop do
